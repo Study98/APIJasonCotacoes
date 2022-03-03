efefeb7f99d4b801ffd83b6c9a3214ef395da529
Study981 <lfcunha.ti@gmail.com>
2022-03-03 00:18:49 -0300

    Criação das Cotações

--- cotacoes.py
+++ cotacoes.py
@@ -3,8 +3,22 @@ import json
 
 cot = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 cot = cot.json()
-cot_euro=cot['EURBRL']
 
 
-print("--------------------------------")
-print("Cotação: {}\n1 EUR equivalem a R$ {}\n\nData e Hora da Requisição (YYYY-MM-DD) (HH:MM:SS): {} ".format(cot_euro['name'],cot_euro['bid'],cot_euro['create_date'] ))
+
+def CotEuro():
+    cot_euro=cot['EURBRL'] #Euro
+    print("--------------------------------")
+    print("Cotação: {}\nEUR 1,00 equivalem a R$ {}\n\nData e Hora da Requisição (YYYY-MM-DD) (HH:MM:SS): {} ".format(cot_euro['name'],cot_euro['bid'],cot_euro['create_date'] ))
+
+def CotDolar():
+    cot_dolar=cot['USDBRL'] #Dolar
+    print("--------------------------------")
+    print("Cotação: {}\nUS$1,00  equivalem a R$ {}\n\nData e Hora da Requisição (YYYY-MM-DD) (HH:MM:SS): {} ".format(cot_dolar['name'],cot_dolar['bid'],cot_dolar['create_date'] ))
+
+def CotBit():
+    cot_btc=cot['BTCBRL'] #Bitcoin
+    print("--------------------------------")
+    print("Cotação: {}\nUS$1,00  equivalem a R$ {}\n\nData e Hora da Requisição (YYYY-MM-DD) (HH:MM:SS): {} ".format(cot_btc['name'],cot_btc['bid'],cot_btc['create_date'] ))
+
+
